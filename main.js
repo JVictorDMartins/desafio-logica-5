4113aeaffa708ca5137ba7b8bf8b0fa6a85f5573
João Victor <jvictordmartins@gmail.com>
2021-05-26 21:39:58 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,32 @@
+ // Crie um array que receba 5 itens e exiba no console.
+
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+
+// Utilize um método para remover o último nome do array.
+
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+
+// Utilize um método para remover o primeiro nome do array.
+
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+
+
+let joãos = ["João","Juvitu","Juvitin","Jubitin","Jãozin"];
+console.log(joãos)
+joãos.pop()
+joãos.shift()
+joãos.unshift("Jubitin")
+joãos.push("Juvitin","João")
+
+
+
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers)
+numbers.sort()
